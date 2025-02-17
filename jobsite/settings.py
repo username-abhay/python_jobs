181f4308245d97f023defad336a40c63dbf21596
Abhay Gupta <abhaay.gupta9935@gmail.com>
2025-02-17 21:13:20 +0530

    added whitenoise

--- jobsite/settings.py
+++ jobsite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -99,7 +100,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'jobs/static'),  # Your custom static files for the project
 ]
