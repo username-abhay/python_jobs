a27df37b582161488d7fb65ad346fe8de4392555
Abhay Gupta <abhaay.gupta9935@gmail.com>
2025-02-17 19:46:20 +0530

    changes

--- jobsite/settings.py
+++ jobsite/settings.py
@@ -11,6 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+SECURE_SSL_REDIRECT = False
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key-for-dev')
 
@@ -66,6 +69,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'CLIENT': {
+            'host': os.getenv('MONGODB_URI', 'mongodb://localhost:27017/'),}
     }
 }
 
